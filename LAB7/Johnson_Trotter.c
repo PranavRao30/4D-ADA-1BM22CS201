67332d7f203e6aa147acf0ba236cd0c2b168b445
PranavRao30 <153255083+PranavRao30@users.noreply.github.com>
2024-06-20 09:15:58 +0530

    Update Johnson_Trotter.c

--- LAB7/Johnson_Trotter.c
+++ LAB7/Johnson_Trotter.c
@@ -1,103 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-int flag = 0;
-int swap(int *a,int *b)
-{
-    int t =*a;
-    *a = *b;
-    *b = t;
-}
-int search(int arr[],int num,int mobile)
-{
-    int g;
-    for(g=0; g<num; g++) {
-        if(arr[g] == mobile)
-            return g+1;
-        else             {
-            flag++;
-        }
-    }
-    return -1;
-}
 
-int find_Moblie(int arr[],int d[],int num)
-{
-    int mobile = 0;
-    int mobile_p =0;
-    int i;
-    for(i=0; i<num; i++) {
-        if((d[arr[i]-1] == 0) && i != 0) {
-            if(arr[i]>arr[i-1] && arr[i]>mobile_p) {
-                mobile = arr[i];
-                mobile_p = mobile;
-            }             else             {
-                flag++;
-            }
-        } else if((d[arr[i]-1] == 1) & i != num-1) {
-            if(arr[i]>arr[i+1] && arr[i]>mobile_p) {
-                mobile = arr[i];
-                mobile_p = mobile;
-            }             else             {
-                flag++;
-            }
-        }         else {
-            flag++;
-        }
-    }
-    if((mobile_p == 0) && (mobile == 0))         return 0;
-    else         return mobile;
-}
+struct ele{
+    int num;
+    int dir;
+};
 
-void permutations(int arr[],int d[],int num)
-{
-    int i;
-    int mobile = find_Moblie(arr,d,num);
-    int pos = search(arr,num,mobile);
-    if(d[arr[pos-1]-1]==0)         swap(&arr[pos-1],&arr[pos-2]);
-    else
-        swap(&arr[pos-1],&arr[pos]);
-    for(int i=0; i<num; i++) {
-        if(arr[i] > mobile) {
-            if(d[arr[i]-1]==0)                 d[arr[i]-1] = 1;
-            else                 d[arr[i]-1] = 0;
+void main(){
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    struct ele e[n];
+    printf("Enter sorted elements: ");
+    for(int i = 0; i < n; i++){
+        scanf("%d", &e[i].num);
+        e[i].dir = 1;
+    }
+    while(1){
+        for(int i = 0; i < n; i++){
+            printf("%d ", e[i].num);
         }
-    }
-    for(i=0; i<num; i++) {
-        printf(" %d ",arr[i]);
-    }
-}
-
-int factorial(int k)
-{
-    int f = 1;
-    int i = 0;
-    for(i=1; i<k+1; i++) {
-        f = f*i;
-    }
-    return f;
-}
-int main()
-{
-    int num =0;
-    int i;
-    int j;
-    int z =0;
-    printf("Johnson trotter algorithm to find all permutations of given numbers \n");
-    printf("Enter the number\n");
-    scanf("%d",&num);
-    int arr[num],d[num];
-    z = factorial(num);
-    printf("total permutations = %d",z);
-    printf("\nAll possible permutations are: \n");
-    for(i=0; i<num; i++) {
-        d[i] = 0;
-        arr[i] = i+1;
-        printf(" %d ",arr[i]);
-    }
-    printf("\n");
-    for(j=1; j<z; j++) {
-        permutations(arr,d,num);
         printf("\n");
+        int curri = -1;
+        for(int i = 0; i < n; i++){
+            if(curri == -1 || e[i].num > e[curri].num){
+                if(e[i].dir == 1){
+                    if(i < n - 1 && e[i+1].num < e[i].num){
+                        curri = i;
+                    }
+                }
+                else{
+                    if(i > 0 && e[i].num > e[i-1].num){
+                        curri = i;
+                    }
+                }
+            }
+        }
+        if(curri == -1) break;
+        struct ele temp = e[curri];
+        if(e[curri].dir == 1){
+            e[curri] = e[curri + 1];
+            e[curri + 1] = temp;
+        }
+        else{
+            e[curri] = e[curri - 1];
+            e[curri - 1] = temp;
+        }
+        for(int i = 0; i < n; i++){
+            if(e[i].num > temp.num){
+                e[i].dir = !e[i].dir;
+            }
+        }
     }
-    return 0;
-}
